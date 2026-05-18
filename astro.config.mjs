c58a2bc6b1883e8aa932b0502eee1dacfe4ac0c5
George Carol <georgecarol210896@gmail.com>
2026-05-18 17:50:31 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://georgecarol210896.github.io',
+  base: '/carol_s_manuscript/',
   integrations: [tailwind()],
+  output: 'static'
 });
